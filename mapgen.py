11c18668a1236d650dfa2dafbeb7f7de344721b3
gilchristad <agilchr8@uwo.ca>
2018-12-19 16:05:23 -0500

    Map Gen almost finished

--- mapgen.py
+++ mapgen.py
@@ -12,21 +12,24 @@ print(len(randomints))
 f = open("map.txt", "w")
 t = 0
 for i in range(0,30):
+	flag = 0
 	row = []
 	length = i + (i + 1)
 	for x in range(0,length):
 		if x == 0 or x == (length - 1):
 			row.append("/")
-		elif i >= (30 - (len(randomints)/2)):
-			# if x <= ((length-1)/2 - (round(randomints[t] / 2))):
-			# 	row.append("*")
-			# elif x >= ((length-1)/2 + (round(randomints[t] / 2))):
-			# 	row.append("*")
-			# else:
-			row.append("&")
-			t += 1
+		elif i >= (30 - (len(randomints)/2)) and t < len(randomints):
+			if x <= (round(length-1)/2) - round(randomints[t]/2):
+				row.append("*")
+			elif x >= (round(length-1)/2) + round(randomints[t]/2):
+				row.append("*")
+			else:
+				row.append("&")
+				flag = 1
 		else:
 			row.append("*")
+	if flag == 1:
+		t += 1
 		
 	map.append(row)
 	f.write(str(row) + "\n")
@@ -39,9 +42,17 @@ for i in range(30,59):
 	for x in range(0,length):
 		if x == 0 or x == (length - 1):
 			row.append("/")
-		elif i <= (31 + (round(len(randomints)/2) -2)):
-			row.append("&")
+		elif i <= (31 + (round(len(randomints)/2) -2)) and t < len(randomints):
+			if x <= (round(length-1)/2) - round(randomints[t]/2):
+				row.append("*")
+			elif x >= (round(length-1)/2) + round(randomints[t]/2):
+				row.append("*")
+			else:
+				row.append("&")
+				flag = 1
 		else:
 			row.append("*")
+	if flag == 1:
+		t += 1
 	map.append(row)
 	f.write(str(row) + "\n")
