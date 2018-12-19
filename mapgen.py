d48258cbfcd8463a155795a0877ba5e439582cce
gilchristad <agilchr8@uwo.ca>
2018-12-19 17:19:28 -0500

    Slight Update

--- mapgen.py
+++ mapgen.py
@@ -6,21 +6,21 @@ randomints = []
 #Generate random number of tiles: the length of the array is how many rows, each number in each index is the number of tiles for that row
 for t in range(random.randint(15,25)):
  		randomints.append(random.randint(7,20))
-print(randomints)
-print(len(randomints))
+
 f = open("mapstrings.txt", "w")
 t = 0
+
 #Iterates the first half of the tiles in the Y direction
-for i in range(0,30):
+for y in range(0,30):
 	flag = 0
 	row = []
-	length = i + (i + 1)
+	length = y + (y + 1)
 	#Iterates the X direction, setting edges as "/", water or impassible terrain as "*", and land as "&"
 	for x in range(0,length):
 		if x == 0 or x == (length - 1):
 			row.append("/")
 		#At half of the Y length of the map, create an island type land mass
-		elif i >= (30 - (len(randomints)/2)) and t < len(randomints):
+		elif y >= (30 - (len(randomints)/2)) and t < len(randomints):
 			if x <= (round(length-1)/2) - round(randomints[t]/2) - 1:
 				row.append("*")
 			elif x >= (round(length-1)/2) + round(randomints[t]/2):
@@ -37,16 +37,16 @@ for i in range(0,30):
 
 #Iterates the second half of the tiles in the Y direction
 j = 1
-for i in range(30,59):
+for y in range(30,59):
 	row = []
-	length = (i+(i+1) - (4 * j))
+	length = (y+(y+1) - (4 * j))
 	j += 1
 	#Iterates the X direction, setting edges as "/", water or impassible terrain as "*", and land as "&"
 	for x in range(0,length):
 		if x == 0 or x == (length - 1):
 			row.append("/")
 		#At half of the Y length of the map, create an island type land mass
-		elif i <= (31 + (round(len(randomints)/2) -2)) and t < len(randomints):
+		elif y <= (31 + (round(len(randomints)/2) -2)) and t < len(randomints):
 			if x <= (round(length-1)/2) - round(randomints[t]/2) -1:
 				row.append("*")
 			elif x >= (round(length-1)/2) + round(randomints[t]/2):
